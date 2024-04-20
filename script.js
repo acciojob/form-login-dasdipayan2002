655f0abea272da701148bb6d349711fd45557e87
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-20 10:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var firstName = document.getElementById('first_name').value;
+    var lastName = document.getElementById('last_name').value;
+    alert(firstName + ' ' + lastName);
+});
 
 }
